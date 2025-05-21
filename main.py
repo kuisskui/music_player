eda672a0b235b2e6fee62b713c572b55a77e535a
Kaopong Sangkittiyakorn <84172946+kuisskui@users.noreply.github.com>
2025-05-22 01:59:34 +0700

    add default music

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
+import os
+from model.media import Media
 from ui.application_ui import ApplicationUI
 from model.cache_manager import CacheManager
 from model.player import player
 
+
+if not os.path.exists(Media.CACHE_FILE):
+    media = Media.build("https://www.youtube.com/watch?v=Alh4nsKYjS4")
+    player.get_playlist().add_media(media)
 
 cache_manager = CacheManager()
 media_list = cache_manager.fetch_media()
